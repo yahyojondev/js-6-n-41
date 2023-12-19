2157e6c78d42f66c95fa5ca6a2ae42760c293ad4
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-20 04:13:25 +0500

    cb fk

---
+++ function.js
@@ -0,0 +1,40 @@
+// function 1
+
+// function power(a,n){
+//     let pow = a ** n;
+//     return pow;
+// }
+//  p = power(3,2);
+
+//  console.log(p);
+
+// function 2
+
+// function mean(a,b){
+//     let A = (a + b) / 2;
+//     let B = Math.sqrt(a + b)
+//     console.log(A);
+//     console.log(B);
+// }
+//   arf = mean(2,3);
+
+// function 3
+
+// function sign(n){
+//     if(n > 0){
+//         console.log(1);
+//     }if(n < 0){
+//         console.log(-1);
+//     }
+// }
+// sign(n) = 2;
+
+// function 4
+
+// function arecircle(R){
+//     let pi = 3.14;
+//     let Y = (pi * R ** 2);
+
+// }
+// d = arecircle(5);
+// console.log(d);
